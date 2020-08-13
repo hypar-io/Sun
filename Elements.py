f3e2c17d5f35ea2b09fa68df7b541743dd357e00
Ian Keough <ian@hypar.io>
2020-08-13 10:28:28 -0700

    First commit.

---
+++ Elements.py
@@ -0,0 +1,142 @@
+import uuid
+
+
+class Bezier(object):
+    # public Bezier(List<Vector3> controlPoints, FrameType frameType = FrameType.Frenet)
+    def __init__(self, controlPoints, frameType='Frenet'):
+        if controlPoints is not None and len(controlPoints) > 0:
+            self.ControlPoints = controlPoints
+        else:
+            self.ControlPoints = []
+        if frameType is not None:
+            self.FrameType = frameType
+        self.discriminator = 'Elements.Geometry.Bezier'
+
+
+class Vector3(object):
+    def __init__(self, x, y, z):
+        self.X = x
+        self.Y = y
+        self.Z = z
+
+
+class Line(object):
+    def __init__(self, start, end):
+        self.Start = start
+        self.End = end
+        self.discriminator = 'Elements.Geometry.Line'
+
+
+class Polygon(object):
+    def __init__(self, vertices):
+        if vertices is not None and len(vertices) > 0:
+            self.Vertices = vertices
+        else:
+            self.Vertices = []
+        self.discriminator = 'Elements.Geometry.Polygon'
+
+
+class Element(object):
+    def __init__(self, id=None, name=None):
+        if id is not None:
+            self.Id = id
+        else:
+            self.Id = str(uuid.uuid4())
+        self.Name = name
+
+
+class Color():
+    def __init__(self, red, green, blue, alpha):
+        self.Red = red
+        self.Green = green
+        self.Blue = blue
+        self.Alpha = alpha
+
+
+class Material(Element):
+    def __init__(self, name=None,  color=None):
+        Element.__init__(self)
+        self.discriminator = 'Elements.Material'
+        if name is not None:
+            self.Name = name
+        else:
+            self.Name = 'default'
+        self.Name = name
+        if color is not None:
+            self.Color = color
+        else:
+            self.Color = Color(1.0, 0.0, 0.0, 0.0)
+        self.SpecularFactor = 0.0
+        self.GlossinessFactor = 0.0
+        self.Unlit = False
+        self.DoubleSided = False
+
+
+class ModelCurve(Element):
+    def __init__(self, curve, material):
+        Element.__init__(self)
+        self.discriminator = 'Elements.ModelCurve'
+        self.Curve = curve
+        if material is not None:
+            self.Material = material
+        else:
+            self.Material = Material()
+        self.Transform = Transform(Matrix(None))
+        self.IsElementDefinition = False
+        self.Representation = None
+
+
+class ModelPoints(Element):
+    def __init__(self, locations, material):
+        Element.__init__(self)
+        self.discriminator = 'Elements.ModelPoints'
+        self.Locations = locations
+        if material is not None:
+            self.Material = material
+        else:
+            self.Material = Material()
+        self.Transform = Transform(Matrix(None))
+        self.IsElementDefinition = False
+        self.Representation = None
+
+
+class Model(object):
+    def __init__(self, transform, elements):
+        if transform is not None:
+            self.Transform = transform
+        else:
+            self.Transform = transform
+        if elements is not None:
+            self.Elements = elements
+        else:
+            self.Elements = elements
+
+    def add_element(self, element):
+        self.Elements[element.Id] = element
+
+
+class Transform(object):
+    def __init__(self, matrix):
+        if matrix is not None:
+            self.Matrix = matrix
+        else:
+            self.Matrix = matrix
+
+
+class Matrix(object):
+    def __init__(self, components):
+        if components is not None:
+            self.Components = components
+        else:
+            self.Components = [1.0,
+                               0.0,
+                               0.0,
+                               0.0,
+                               0.0,
+                               1.0,
+                               0.0,
+                               0.0,
+                               0.0,
+                               0.0,
+                               1.0,
+                               0.0]
---
+++ Sun.py
@@ -0,0 +1,46 @@
+import Elements
+from ladybug.location import Location
+from ladybug.sunpath import Sunpath
+import json
+import math
+import sys
+
+
+def execute(models, inputs):
+    # Create location. You can also extract location data from an epw file.
+    sydney = Location('Sydney', 'AUS', latitude=-33.87,
+                      longitude=151.22, time_zone=10)
+
+    # Initiate sunpath
+    sp = Sunpath.from_location(sydney)
+
+    radius = 100
+
+    material = Elements.Material(
+        'day_color', Elements.Color(1.0, 1.0, 0.0, 1.0))
+    model = Elements.Model(Elements.Transform(Elements.Matrix(None)), {})
+    model.add_element(material)
+
+    for month in range(1, 12):
+        for day in range(1, 28):
+            vertices = []
+            for hour in range(0, 24):
+                sun = sp.calculate_sun(month=month, day=day, hour=hour)
+                azimuth = sun.azimuth_in_radians
+                elevation = sun.altitude_in_radians
+
+                # https://en.wikipedia.org/wiki/Spherical_coordinate_system
+                x = radius * math.cos(elevation) * math.sin(azimuth)
+                y = radius * math.cos(elevation) * math.cos(azimuth)
+                z = radius * math.sin(elevation)
+                vertices.append(Elements.Vector3(x, y, z))
+
+            model_curve = Elements.ModelPoints(vertices, material.Id)
+            model.add_element(model_curve)
+
+    model_json = json.dumps(model, default=lambda x: x.__dict__)
+    # print(model_json)
+    # out = open("test.json", "w")
+    # out.write(model_json)
+    # out.close()
+    return {'Area': 0, 'Model': model_json}
